b375bb27384c071638d2d7dd6d17f9214bb0d335
Aditya Gaikwad <adu.gaikwad9552@gmail.com>
2025-05-04 16:00:37 +0530

    Initial commit with Git LFS tracking

---
+++ pytorch_i3d.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c322c3ca015dab5ee43e4d15707149d510b98931406c4a5d58b27934ed6777ed
+size 13801
---
+++ streamlit_app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f30bc4ed42faf692d109cbcf71481c8b067a4826f8bffb28e610207c2291add
+size 13876
